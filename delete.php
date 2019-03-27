8a318d326e2b1579c1e0b888ffeb124c35c49818
Pechckin <kulesh14@icloud.com>
2019-03-27 14:50:55 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -2,16 +2,12 @@
   header("Content-type: text/json; charset=utf-8");
   include "bd.php";
 
-    //$del =  mysqli_query($db,"DELETE FROM `phone_book` WHERE 'phone' =  '" . $_POST["phone"] . "')");
- $del =  mysqli_query($db,"DELETE FROM `phone_book` WHERE 'phone' = 4" ));
- //$del =  mysqli_query($db,"INSERT INTO `phone_book`(`surname`, `name`, `phone`) VALUES (".
-//                            "'" . $_POST["surname"] . "'," .
-//                            "'" . $_POST["name"] . "'," . 
-//                            "'" . $_POST["phone"] . "')");
-    //die( "DELETE FROM `phone_book` WHERE `phone` = '" . $_POST["phone"] . "')");
+ $del =  mysqli_query($db,"DELETE FROM `phone_book` WHERE `phone` =  '" . $_POST["phone"] . "'");
     if ($del);
     else die('<p>delete error.</p>');
 
     
  ?>
+        
+
         
